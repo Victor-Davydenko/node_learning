0ff8388c0bcc472149828f6a6390e85b094bedb8
Victor Davydenko <acc.davydenko@gmail.com>
2024-02-09 16:32:05 +0200

    add check existance of directory to save file

--- src/downloadFileTask/downloadFile.ts
+++ src/downloadFileTask/downloadFile.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import fsPromise from 'fs/promises';
 import path from 'path';
+import fs from 'fs';
 
 const hw3DownloadImageTask = () => {
 
@@ -9,14 +10,31 @@ const hw3DownloadImageTask = () => {
     return  url.pathname.split('/').slice(-1).join('');
   };
 
+  const checkFolderExists = async (pathToStore) => {
+    try {
+      await fsPromise.access(pathToStore);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  const createFolder = (path) => {
+    fs.mkdirSync(path);
+  };
+
   const processImage = async (pathFrom: string, pathToStore: string) => {
     const fileNameWithExtension = getFileNameFromURL(pathFrom);
+    const isDirectroryToStoreExists = await checkFolderExists(pathToStore);
+    if (!isDirectroryToStoreExists) {
+      createFolder(pathToStore);
+    }
     try {
       const response = await axios.get(pathFrom, { responseType: 'arraybuffer' });
       await fsPromise.writeFile(path.join(pathToStore, fileNameWithExtension), response.data);
       return `Image successfully stored to ${pathToStore}`;
     } catch (e) {
-      throw new Error('Something went wrong');
+      throw new Error(e);
     }
   };
 
