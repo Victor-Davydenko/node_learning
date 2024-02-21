8477240323d0738b6e209798d5b7b1cad028661f
Victor Davydenko <acc.davydenko@gmail.com>
2024-02-21 13:56:21 +0200

    hw 7 done

--- src/routes/userRouter/userRouter.ts
+++ src/routes/userRouter/userRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import UserController from '../../controllers/userController';
+import { validationMiddleware } from '../../middlwares/validationMiddlware';
+import { createUserValidationSchema, loginUserValidationSchema } from '../../validationSchemas/validationSchemas';
 
 const userRouter = Router();
 
-userRouter.get('/api/v1/users', UserController.getUsers);
-userRouter.post('/user', UserController.createUser);
+userRouter.get('/signin', validationMiddleware(loginUserValidationSchema), UserController.login);
+userRouter.post('/signup', validationMiddleware(createUserValidationSchema), UserController.createUser);
 
 export default userRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { json } from 'express';
 import router from './routes';
+import { errorMiddlware } from './middlwares/errorMiddlware';
 
 const app = express();
 
 app.use(json());
 app.use('/', router);
+app.use(errorMiddlware);
 
 export default app;
