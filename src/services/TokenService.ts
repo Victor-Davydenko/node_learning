6ccecab6ed879f485adb02fbf1f5957f9ff11571
Victor Davydenko <acc.davydenko@gmail.com>
2024-03-01 14:51:33 +0200

    remove unnecessary code

--- src/services/TokenService.ts
+++ src/services/TokenService.ts
@@ -1,7 +1,6 @@
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 class TokenService {
-  tokens = [];
   constructor () {
   }
 
@@ -12,24 +11,6 @@ class TokenService {
       accessToken,
       refreshToken
     };
-  };
-
-  // verifyToken = (token, secretKey) => {
-  //   const userData = jwt.verify(token, secretKey) as JwtPayload;
-  //   return userData;
-  // };
-
-  findToken = (token) => {
-    const tokenFromDB = this.tokens.find((tokenFromDB) => tokenFromDB === token);
-    return tokenFromDB;
-  };
-
-  saveToken = (token) => {
-    this.tokens.push(token);
-  };
-
-  removeToken = (token) => {
-    this.tokens = this.tokens.filter((tokenFromDB) => tokenFromDB !== token);
   };
 }
 
