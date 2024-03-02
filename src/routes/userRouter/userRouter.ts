0976fc5d607ac785bb7151a99900a1e784d065e0
Victor Davydenko <acc.davydenko@gmail.com>
2024-03-02 14:48:24 +0200

    router method fix

--- src/routes/userRouter/userRouter.ts
+++ src/routes/userRouter/userRouter.ts
@@ -6,7 +6,7 @@ import authMiddlware from '../../middlwares/authMiddlware';
 
 const userRouter = Router();
 
-userRouter.get('/signin', validationMiddleware(loginUserValidationSchema), UserController.login);
+userRouter.post('/signin', validationMiddleware(loginUserValidationSchema), UserController.login);
 userRouter.post('/signup', validationMiddleware(createUserValidationSchema), UserController.createUser);
 userRouter.get('/api/auth/refresh-token',authMiddlware('jwt_refresh'), UserController.refreshToken);
 userRouter.get('/profile', authMiddlware('jwt_access'), UserController.getProfile);
